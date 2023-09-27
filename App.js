919a07664ea3bba1a39ae05f72be246f0b351dbc
Nikolay Shein <qseboy1998@gmail.com>
2023-09-27 18:47:47 +0300

    init project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Mykola Shein 27.09.2023 Expo Go + Android Studio</Text>
       <StatusBar style="auto" />
     </View>
   );
