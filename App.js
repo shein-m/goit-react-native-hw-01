b77aeac4935172d38f9158442fc881be4144b3bb
Nikolay Shein <qseboy1998@gmail.com>
2023-09-28 19:51:12 +0300

    add registration screen with layout and styles

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { RegistrationScreen } from './Screens/RegistrationScreen/RegistrationScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Mykola Shein 27.09.2023 Expo Go + Android Studio</Text>
-      <StatusBar style="auto" />
+      <RegistrationScreen />
     </View>
   );
 }
@@ -13,8 +12,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/images/svg/add.js
@@ -0,0 +1,6 @@
+export const svgSprite = {
+  add: ` <svg width="25" height="25" viewBox="0 0 32 32">
+    <path fill="#fff" style="fill: var(--color1, #fff); stroke: var(--color2, #ff6c00)" stroke="#ff6c00" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="1.28" d="M31.36 16c0 8.483-6.877 15.36-15.36 15.36s-15.36-6.877-15.36-15.36c0-8.483 6.877-15.36 15.36-15.36s15.36 6.877 15.36 15.36z"></path>
+    <path fill="#ff6c00" style="fill: var(--color2, #ff6c00)" d="M16.64 7.68h-1.28v7.68h-7.68v1.28h7.68v7.68h1.28v-7.68h7.68v-1.28h-7.68v-7.68z"></path>
+  </svg>`,
+};
